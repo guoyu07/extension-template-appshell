eff7be9fcea5337495f4e7dcf6c4e6187a729b83
Wangyisheng <wangyisheng@baidu.com>
2017-08-28 16:00:20 +0800

    初始化appshell

---
+++ recommend/store.js
@@ -0,0 +1,20 @@
+/**
+ * @file store
+ * @author *__ author __*{% if: *__ email __* %}(*__ email __*){% /if %}
+ */
+
+'use strict';
+
+import Vue from 'vue';
+import Vuex from 'vuex';
+import {createAppShellState} from 'extensions/appShell/store/module';
+
+Vue.use(Vuex);
+
+export function createStore() {
+    return new Vuex.Store({
+        modules: {
+            appShell: createAppShellState()
+        }
+    });
+}
